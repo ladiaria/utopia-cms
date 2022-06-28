96ad2a80503c0d315c63d046080f41995cd05119
Aníbal Pacheco <apacheco@ladiaria.com.uy>
2022-06-28 01:20:58 -0300

    syntax code style

--- portal/apps/dashboard/urls.py
+++ portal/apps/dashboard/urls.py
@@ -14,23 +14,15 @@ tables = (
     'audio_statistics',
     'categories',
     'article_views',
-    'audio_statistics'
+    'audio_statistics',
 )
 
 formatted_tables = "|".join(table_name for table_name in tables)
 
 urlpatterns = [
     url(r'^$', index, name="index"),
-    url(
-        r'^export/(?P<table_id>{})/$'.format(formatted_tables),
-        export_csv,
-        name="export_csv",
-    ),
-    url(
-        r'^table/(?P<table_id>{})/$'.format(formatted_tables),
-        load_table,
-        name="load_table",
-    ),
+    url(r'^export/(?P<table_id>{})/$'.format(formatted_tables), export_csv, name="export_csv"),
+    url(r'^table/(?P<table_id>{})/$'.format(formatted_tables), load_table, name="load_table"),
     url(r'^audio_statistics_api/$', audio_statistics_api, name="audio_statistics_api"),
     url(r'^audio_statistics_api_amp/$', audio_statistics_api_amp, name="audio_statistics_api_amp"),
 ]
