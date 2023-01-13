fee0a545592844fa0d831b27096b1f4b15d9b1fb
Aníbal Pacheco <apacheco@ladiaria.com.uy>
2023-01-13 02:14:54 -0300

    syntax code style

--- portal/apps/core/admin.py
+++ portal/apps/core/admin.py
@@ -300,15 +300,13 @@ class SolanaSignatureWidget(Widget):
         super(SolanaSignatureWidget, self).__init__(*args, **kwargs)
 
     def get_context(self, name, value, attrs=None):
-        return {'widget': {
-            'name': name,
-            'value': value,
-        }}
+        return {'widget': {'name': name, 'value': value}}
 
     def render(self, name, value, attrs=None):
         context = self.get_context(name, value, attrs)
         template = loader.get_template(self.template_name).render(context)
         return mark_safe(template)
+
 
 class SolanaSignatureField(Field):
     def __init__(self, field, *args, **kwargs):
