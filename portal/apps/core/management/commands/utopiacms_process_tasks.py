235087393d17c83eddd98550ee80e6ec790f3f88
Aníbal Pacheco <apacheco.uy@gmail.com>
2023-06-19 04:53:17 -0300

    useful wrapper+ todo's

---
+++ portal/apps/core/management/commands/utopiacms_process_tasks.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# utopia-cms 2023. Aníbal Pacheco.
+
+from MySQLdb import OperationalError
+
+from background_task.management.commands.process_tasks import Command as ProcessTasksCommand
+
+
+class Command(ProcessTasksCommand):
+    help = "Wrapper for process_tasks that also handle exceptions"
+
+    def handle(self, *args, **options):
+        try:
+            super().handle(*args, **options)
+        except OperationalError:
+            pass
