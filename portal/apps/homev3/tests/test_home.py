f19453e091fdb45c0bb7c0684aedef29ce64bc37
Aníbal Pacheco <apacheco.uy@gmail.com>
2024-08-09 18:00:23 -0300

    test_upd

--- portal/apps/homev3/tests/test_home.py
+++ portal/apps/homev3/tests/test_home.py
@@ -82,4 +82,4 @@ class HomeTestCase(TestCase):
         # article_with_iframe_in_extension
         response = c.get('/articulo/2024/07/test-article9/', **self.http_host_header_param)
         self.assertEqual(response.status_code, 200)
-        self.assrtNotIn(self.amp_detection, response.content.decode())
+        self.assertNotIn(self.amp_detection, response.content.decode())
