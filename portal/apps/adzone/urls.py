146216fddf134b71e9b08caeec2a6ecf9afe407a
Aníbal Pacheco <apacheco.uy@gmail.com>
2024-08-23 04:18:30 -0300

    publication extra content new field and other minor improvements

--- portal/apps/adzone/urls.py
+++ portal/apps/adzone/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, re_path
 
-from adzone.views import ad_view, ad_content
+from .views import ad_view, ad_content
+
 
 urlpatterns = [
-    re_path(r'^view/(?P<id>[\d]+)/(?P<tracking>.*)$', ad_view,
-        name='adzone_ad_view'),
+    re_path(r'^view/(?P<id>[\d]+)/(?P<tracking>.*)$', ad_view, name='adzone_ad_view'),
     path('content/', ad_content, name='adzone_ad_content'),
 ]
