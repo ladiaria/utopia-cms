3c61416448ea3e1cd6e258dba3cca69f07869a2a
Agus Vargas <agustinvargas93@gmail.com>
2024-09-19 00:56:41 -0300

    refactor options in int tel widget

--- portal/apps/thedaily/static/js/phone_widget.js
+++ portal/apps/thedaily/static/js/phone_widget.js
@@ -1,19 +1,18 @@
-function phone_widget(local_country, util_script, required=false, no_placeholder=false, translations) {
+function phone_widget(local_country, util_script, required=false, extraOptions=false) {
 
   const input = document.querySelector("#id_phone");
   let options = {
     initialCountry: local_country,
-    i18n: translations,
     hiddenInput: function(phone) {
       return {
         phone: "phone"
       };
     },
-    utilsScript: util_script
+    utilsScript: util_script,
   };
-  if (no_placeholder) {
-    options.placeholderNumberType = false;
-  }
+
+  if (extraOptions) Object.assign(options, extraOptions);
+
   const iti = window.intlTelInput(input, options);
   input.addEventListener("countrychange", function(e) {
     e.target.value = "";
