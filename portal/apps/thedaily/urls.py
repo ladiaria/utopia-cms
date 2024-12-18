012ea0afcb2a8b8109089773fb31fee07114e351
Francisco Cobas <fcarocena@gmail.com>
2024-12-18 17:51:31 -0300

    Change unused space

--- portal/apps/thedaily/urls.py
+++ portal/apps/thedaily/urls.py
@@ -120,8 +120,8 @@ urlpatterns = [
         disable_profile_property,
         name="disable-profile-property",
     ),
-
     path('perfil/<int:user_id>/', user_profile, name="user-profile"),
+
     path('registrate/', signup, name="account-signup"),
     path('registrate/google/', google_phone, name="account-google"),
     path('salir/', logout_view, name="account-logout"),
