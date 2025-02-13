9166b033e383680c119dc2cb7c6d3fbb297ed48f
Aníbal Pacheco <apacheco.uy@gmail.com>
2025-02-13 19:36:06 -0300

    lnbr-

--- portal/apps/thedaily/views.py
+++ portal/apps/thedaily/views.py
@@ -668,7 +668,6 @@ def google_phone(request):
         google_signin_form = GoogleSigninForm(**form_kwargs)
     subscribe_log(request, 'google_phone end')
     ctx.update({'google_signin_form': google_signin_form, "is_new": is_new})
-
     return render(request, get_app_template("google_signup.html"), ctx)
 
 
