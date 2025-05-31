b7255ca2ce4bb018369ed29d8378b953670409f6
Aníbal Pacheco <apacheco.uy@gmail.com>
2025-05-31 04:11:05 -0300

    realocate_upd

--- portal/libs/scripts/one_time/pool_rename_photo.py
+++ portal/libs/scripts/one_time/pool_rename_photo.py
@@ -8,22 +8,34 @@ from django.utils.text import slugify
 from photologue.models import Photo
 
 
-def realocate(filter_kwargs={}):
+def realocate(filter_kwargs={}, verbose=False, dry_run=False):
     target_qs = Photo.objects.filter(**filter_kwargs) if filter_kwargs else Photo.objects
-    moved, total = 0, target_qs.count()
+    non_existent, errors, in_pool, moved, total = 0, 0, 0, 0, target_qs.count()
     for photo in tqdm(target_qs.iterator(), total=total):
         try:
-            if exists(photo.image.path) and dirname(str(photo.image)) == "photologue/photos":
-                pc = deepcopy(photo)
-                filename_splitted = splitext(pc.image_filename())
-                pc.image.save(slugify(filename_splitted[0]) + filename_splitted[1], pc.image.file)
-                unlink(photo.image.path)
+            if exists(photo.image.path):
+                if dirname(str(photo.image)) == "photologue/photos":
+                    if dry_run:
+                        if verbose:
+                            print(f"Would move {photo.image.path} to pool")
+                        moved += 1
+                        continue
+                    pc = deepcopy(photo)
+                    filename_splitted = splitext(pc.image_filename())
+                    pc.image.save(slugify(filename_splitted[0]) + filename_splitted[1], pc.image.file)
+                    unlink(photo.image.path)
+                    moved += 1
+                else:
+                    in_pool += 1
+            else:
+                non_existent += 1
         except Exception as e:
-            print(e)
-        else:
-            moved += 1
+            errors += 1
+            if verbose:
+                print(e)
 
-    print(f"Moved {moved} of {total}")
+    m = "Would move" if dry_run else "Moved"
+    print(f"{m} {moved} of {total} (Non-existent: {non_existent}, Errors: {errors}, Already in pool: {in_pool})")
 
 
 def non_existent_photos():
