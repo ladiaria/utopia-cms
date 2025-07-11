64d248090f8aeedd8b957d317ae0f27fe8acc9f3
Reidel <reidelon@gmail.com>
2025-07-11 18:16:15 -0300

    js and settings improvements to make the google one tap works

--- portal/settings.py
+++ portal/settings.py
@@ -587,6 +587,8 @@ EXCLUDE_ONE_TAP_FOR_URLS = ["/usuarios/entrar/",
                             "/usuarios/suscribite/",
                             "/usuarios/restablecer/"]
 
+
+
 # ====================================================================================== visual separator =============
 
 
