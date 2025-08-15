94210ab2955cb21c27043468cd8384069c3051e3
Reidel <reidelon@gmail.com>
2025-08-15 17:20:32 -0300

    adding query parameter  in order to send information to view

--- portal/apps/thedaily/views.py
+++ portal/apps/thedaily/views.py
@@ -2242,7 +2242,7 @@ def nlunsubscribe(request, publication_slug, hashed_id):
                 error_message = getattr(e, 'displaymessage', str(e))
                 ctx['error'] = error_message
                 return 'nlunsubscribe.html', ctx
-            return redirect('user_newsletters')
+            return redirect(f"{reverse('user_newsletters')}?nl={publication.newsletter_name or publication.name}")
     except IndexError:
         raise Http404
 
@@ -2271,7 +2271,7 @@ def nl_category_unsubscribe(request, category_slug, hashed_id):
                 error_message = getattr(e, 'displaymessage', str(e))
                 ctx['error'] = error_message
                 return 'nlunsubscribe.html', ctx
-        return redirect('user_newsletters')
+        return redirect(f"{reverse('user_newsletters')}?nl={category.name}")
     except IndexError:
         raise Http404
 
