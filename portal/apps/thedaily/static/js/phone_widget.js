f9f594d2328ff4586df819d4099632a6806e7cf5
Francisco Cobas <fcarocena@gmail.com>
2025-10-02 16:38:31 -0300

    Remove comment

--- portal/apps/thedaily/static/js/phone_widget.js
+++ portal/apps/thedaily/static/js/phone_widget.js
@@ -33,7 +33,7 @@ function phone_widget(
         input.setCustomValidity(required && !val || val && !intlTelInput.isValidNumber() ? "Formato incorrecto" : "");
       });
       input.dispatchEvent(new CustomEvent("phone-widget-ready", {
-          detail: { intlTelInput }  // podés pasar la instancia si querés
+          detail: { intlTelInput }
       }));
     }
   });
