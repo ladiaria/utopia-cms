8b8ed6e8e0f479885c9a0de8ee5cfa3040a35fbb
Reidel <reidelon@gmail.com>
2025-11-27 15:04:45 -0300

    adding sentry for BE and FE

--- portal/settings.py
+++ portal/settings.py
@@ -585,6 +585,7 @@ PHONENUMBER_DEFAULT_REGION = None
 CRM_API_HTTP_BASIC_AUTH = None  # Override to tuple (user, pass) if the CRM is restricted using basic auth
 ENV_HTTP_BASIC_AUTH = False  # Override to True if this CMS deployment is restricted using basic auth
 ENABLE_GOOGLE_ONE_TAP = False
+SENTRY_ENABLED = False
 
 # ====================================================================================== visual separator =============
 
