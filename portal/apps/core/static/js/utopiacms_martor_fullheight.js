2c08fe245841916c25aa253ee7bf97f5594da05e
Aníbal Pacheco <apacheco.uy@gmail.com>
2026-03-13 19:16:47 -0300

    checkpoint commit

--- portal/apps/core/static/js/utopiacms_martor_fullheight.js
+++ portal/apps/core/static/js/utopiacms_martor_fullheight.js
@@ -1,21 +1,22 @@
 if (window.jQuery) {
   $(function(){
-    // TODO: if theme is bootstrap + better html for the "button"
-    // we create a float right expand button inside charcounter div and register its click event, this is the only way
-    // we found to allow search natively with ctrl+f cycling with focus on all matches.
-    $(".martor-field").next("textarea").next("div.charcounter")
-    .append("<span class='expand-editor' style='float: right;'>expand</span>").on("click", function(){
+    var runExpand = function () {
       var mbody = $(".martor-field");
       var set_height = 400;  // default
       var max_height = $("div.ace_scrollbar-v div.ace_scrollbar-inner").height();
-      if (max_height > set_height && mbody.height() < max_height) {
-          set_height = max_height;
-      }
+      if (max_height > set_height && mbody.height() < max_height) set_height = max_height;
       mbody.attr("style", "height:" + set_height + "px !important");
       $("div.ace_scrollbar-v").css("height", set_height + "px");
       $("div.ace_content").css("height", set_height + 28 + "px");
       window.dispatchEvent(new Event("resize"));
-    });
+    };
+    // Semantic: button exists in template. Bootstrap: no button, we add one so Ctrl+F can work with native find.
+    if ($(".main-martor .expand-editor").length === 0) {
+      $(".martor-field").next("textarea").next("div.charcounter").append(
+        $("<span>\u21D3</span>").addClass("expand-editor").attr({title: "Expand editor", ariaLabel: "Expand editor"})
+        .css({float: "right", cursor: "pointer"}).on("click", runExpand)
+      );
+    }
     // Ctrl+F / Cmd+F: let browser default (find) run. Capture phase so we run before Ace; stopPropagation so Ace
     // doesn't consume it; we do NOT preventDefault.    //
     // TODO: let this exec only once, a 2nd time makes no sense.
