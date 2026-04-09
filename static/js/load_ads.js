bdb66b79c2dc577c151412be814773621f7e2fb4
Francisco Cobas <fcarocena@gmail.com>
2026-04-09 17:36:09 -0300

    Add cover ad block with CLS-safe placeholder to home templates

--- static/js/load_ads.js
+++ static/js/load_ads.js
@@ -2,5 +2,11 @@ const adEl = document.getElementById('header-pub');
 if (adEl) {
   fetch(adEl.dataset.adUrl)
     .then(r => r.text())
-    .then(html => { adEl.innerHTML = html; });
+    .then(html => {
+      if (html.trim()) {
+        adEl.innerHTML = html;
+      } else {
+        adEl.remove();
+      }
+    });
 }
